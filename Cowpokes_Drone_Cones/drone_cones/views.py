ccf253670375e33f262932269ffb9a4d516cd637
westbm3 <a02307413@usu.edu>
2023-10-16 08:37:55 -0600

    added function calls

--- Cowpokes_Drone_Cones/drone_cones/views.py
+++ Cowpokes_Drone_Cones/drone_cones/views.py
@@ -3,5 +3,49 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from .models import *
 
+def login():
+    pass
 
-# Create your views here.
+def register():
+    pass
+
+def logout():
+    pass
+
+def viewCart():
+    pass
+
+def viewProfile():
+    pass
+
+def userDash():
+    pass
+
+def droneDash():
+    pass
+
+def viewDrones():
+    pass
+
+def editDrones():
+    pass
+
+def adminDash():
+    pass
+
+def viewUsers():
+    pass
+
+def editUsers():
+    pass
+
+def orderView():
+    pass
+
+def editAddress():
+    pass
+
+def addAddress():
+    pass
+
+
