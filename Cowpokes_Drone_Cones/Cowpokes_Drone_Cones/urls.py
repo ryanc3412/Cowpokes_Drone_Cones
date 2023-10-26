a75b652af42c2248ec76afebbfef80849b30889b
westbm3 <a02307413@usu.edu>
2023-10-25 18:42:38 -0600

    added url for dashboard

--- Cowpokes_Drone_Cones/Cowpokes_Drone_Cones/urls.py
+++ Cowpokes_Drone_Cones/Cowpokes_Drone_Cones/urls.py
@@ -22,4 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.LoginView.login, name='login'),
+    path('home/', views.UserView.user_dash, name='user_dash'),
 ]
