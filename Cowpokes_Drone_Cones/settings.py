c5ff29023580a5b477aa0520c2f6c1bb222c8d54
westbm3 <a02307413@usu.edu>
2023-10-30 07:27:44 -0600

    fixed home link

--- Cowpokes_Drone_Cones/settings.py
+++ Cowpokes_Drone_Cones/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "/dronecones/home/"
+LOGIN_REDIRECT_URL = "/dronecones/"
 
 LOGOUT_REDIRECT_URL = "/dronecones/accounts/login/"
