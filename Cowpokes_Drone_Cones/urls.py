2b12cfde51536bdb53ab170969b80fc77bfec886
westbm3 <a02307413@usu.edu>
2023-11-09 23:14:59 -0700

    updated

--- Cowpokes_Drone_Cones/urls.py
+++ Cowpokes_Drone_Cones/urls.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
-# from django.urls import include, path
-
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('drone_cones/', include('drone_cones.urls'))
 from django.urls import path, include
 
 urlpatterns = [
