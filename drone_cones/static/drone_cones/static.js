1b0f3f5853f5022303838d172e8b49b88eefae94
Ethan Ford <ethanfordj5@gmail.com>
2023-11-12 12:53:04 -0700

    updated static file for order page

--- drone_cones/static/drone_cones/static.js
+++ drone_cones/static/drone_cones/static.js
@@ -22,6 +22,64 @@ function timer() {
 	}, 1000);
 }
 
+function writeAddress(checkAddress) {
+    var addressOne = document.getElementById("address1");
+    var addressTwo = document.getElementById("address2");
+
+    // Use checkAddress.checked to check if the checkbox is checked
+    addressTwo.value = checkAddress.checked ? addressOne.value : "";
+}
+
+
+function populate() {
+	const flavorList = [
+		"Chocolate",
+		"Vanilla",
+		"Mint Chocolate Chip",
+		"Rocky Road",
+		"Cookie Dough"
+	];
+
+	const scoopList = [
+		1,
+		2,
+		3
+	]
+
+	const coneList = [
+		"Waffle",
+		"Fudge",
+		"Original"
+	]
+
+	// Get the "flavors" div
+	var flavorsDiv = document.getElementById("flavors");
+	var scoopsDiv = document.getElementById("scoops");
+	var conesDiv = document.getElementById("coneflavors");
+
+	// Create a div for each flavor and append it to the "flavors" div
+	for (var i = 0; i < flavorList.length; i++) {
+		var flavorDiv = document.createElement("div");
+		flavorDiv.setAttribute("class", "productDisplay");
+		flavorDiv.setAttribute("onclick", "select(this)")
+		flavorDiv.textContent = flavorList[i];
+		flavorsDiv.appendChild(flavorDiv);
+	}
+
+	for (var i = 0; i < scoopList.length; i++){
+		var scoopDiv = document.createElement("div");
+		scoopDiv.setAttribute("class", "productDisplay");
+		scoopDiv.textContent = scoopList[i];
+		scoopsDiv.appendChild(scoopDiv);
+	}
+
+	for (var i = 0; i < coneList.length; i++){
+		var coneDiv = document.createElement("div");
+		coneDiv.setAttribute("class", "productDisplay");
+		coneDiv.textContent = coneList[i];
+		conesDiv.appendChild(coneDiv);
+	}
+}
 
 
 
