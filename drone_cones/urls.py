5d5f99170e21ad290b13748ede148a5683af8444
Ryan Christensen <a023107782@usu.edu>
2023-12-06 17:56:36 -0700

    edit stock form

--- drone_cones/urls.py
+++ drone_cones/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('manager/all_drones/', views.ManagerView.view_drones, name='all_drones'),
     path('manager/users/edit_user/<int:account_id>', views.ManagerView.edit_user, name="edit_user"),
     path('manager/all_drones/edit_drone/<int:drone_id>', views.ManagerView.edit_drone, name="edit_drone_manager"),
+    path('manager/stock/edit_stock/<int:product_id>', views.ManagerView.edit_stock, name="edit_stock"),
     path('get_products/', views.OrderView.get_products, name='get_products'),
     path('add_to_cart/', views.OrderView.add_to_cart, name='add_to_cart'),
     path('send_order/', views.OrderView.send_order, name='send_order'),
